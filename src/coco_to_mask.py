d9eedae56d35d7ddc838d8eed0296badd8c5d829
Fiona Fung <ffung.dev@gmail.com>
2026-08-04 14:50:17 -0400

    coco json files are in the train folders

--- src/coco_to_mask.py
+++ src/coco_to_mask.py
@@ -4,3 +4,10 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+roboflow_folder = Path('../data/masks_roboflow')
+mask_folder = Path('../data/processed')
+
+for morphology_folder in roboflow_folder.iterdir():
+    if not morphology_folder.is_dir(): #ignore anything that isn't a folder
+        continue 
+    
