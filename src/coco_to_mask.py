cc8d204c5bade1fe5a219efeb01364b4ea6b140e
Fiona Fung <ffung.dev@gmail.com>
2026-08-05 11:39:38 -0400

    coco -> segmented img

--- src/coco_to_mask.py
+++ src/coco_to_mask.py
@@ -4,10 +4,40 @@ from pathlib import Path
 import cv2
 import numpy as np
 
-roboflow_folder = Path('../data/masks_roboflow')
-mask_folder = Path('../data/processed')
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+roboflow_folder = PROJECT_ROOT / "data" / "masks_roboflow"
+mask_folder = PROJECT_ROOT / "data" / "processed"
 
 for morphology_folder in roboflow_folder.iterdir():
     if not morphology_folder.is_dir(): #ignore anything that isn't a folder
         continue 
-    
+
+    input_folder = morphology_folder / 'train'
+    output_folder = mask_folder / morphology_folder.name 
+
+    # from coco -> mask
+    with open(input_folder / '_annotations.coco.json', 'r') as file:
+        coco = json.load(file)
+    images = {}
+    for image in coco['image']:
+        images[image['id']] = image
+    for image_id, image_info in images.items():
+        height = image_info['height']
+        width = image_info['width']
+        # blank mask
+        mask = np.zeros((height,width), dtype=np.uint8)
+
+        for ann in coco['annotations']:
+            if ann['image_id'] != image_id:
+                continue
+
+            for polygon in ann['segmentation']:
+                points = np.array(polygon).reshape(-1,2)
+                points = points.astype(np.int32)
+
+                cv2.fillPoly(mask, [points], 255)
+
+        filename = Path(image_info['file_name']).stem + '.png'
+        cv2.imwrite(str(output_folder / filename), mask)
+
+print('fin')
